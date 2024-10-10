ec7700c8cf6a6b388f9e0aaa6e9974c796c042b9
GeeEq <sielaleis@gmail.com>
2024-10-10 15:06:28 +0300

    changed layout and adjusting image and card styles

--- src/App.jsx
+++ src/App.jsx
@@ -12,38 +12,37 @@ function App() {
 <div className='container'>
   <div className='card'> 
 
-     <img src="https://www.ultimatekilimanjaro.com/wp-content/uploads/2023/08/elbrus.jpg" alt="mountain" />
-
-     <div className='overlay'> 
+     <img src="https://www.ultimatekilimanjaro.com/wp-content/uploads/2023/08/elbrus.jpg" alt="mountain" className='card-img'/>
+    <div className='card-body'>      
       <h5>Mountain Tops</h5>
       <p>Best place to be for peaceful minds</p>
-     </div>
+    </div>
   </div>
   <div className='card'>
-    <img src="https://i0.wp.com/epthinktank.eu/wp-content/uploads/2018/07/fotolia_169832225_subscription_xxl.jpg?fit=1000%2C666&ssl=1" alt="mountain" /> 
-    <div className='overlay'> 
+    <img src="https://i0.wp.com/epthinktank.eu/wp-content/uploads/2018/07/fotolia_169832225_subscription_xxl.jpg?fit=1000%2C666&ssl=1" alt="mountain" className='card-img'/> 
+    <div className='overlay2'> 
      <h5>Mountain Tops</h5>
      <p>Best place to be for peaceful minds</p>
     </div>
 </div>
   <div className='card'>
-    <img src="https://s1.travix.com/blog/eu/europe-switzerland-autumn-zermatt-matterhorn-lake-medium.jpg" alt="mountain" /> 
-     <div className='overlay'> 
+    <img src="https://s1.travix.com/blog/eu/europe-switzerland-autumn-zermatt-matterhorn-lake-medium.jpg" alt="mountain" className='card-img'/> 
+     <div className='overlay3'> 
        <h5>Mountain Tops</h5>
        <p>Best place to be for peaceful minds</p>
      </div>
 </div>
   <div className='card'>
-    <img src="https://www.thoughtco.com/thmb/KAc9b99sev8BSY25Od7PqpT61R0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-841965988-59c67827685fbe0011219921.jpg" alt="mountain" />  
-    <div className='overlay'>  
+    <img src="https://www.thoughtco.com/thmb/KAc9b99sev8BSY25Od7PqpT61R0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-841965988-59c67827685fbe0011219921.jpg" alt="mountain" className='card-img'/>  
+    <div className='overlay4'>  
       <h5>Mountain Tops</h5>
       <p>Best place to be for peaceful minds</p>
     </div>
 </div>
   <div className='card'>
-    <img src="https://dinoanimals.com/wp-content/uploads/2022/04/Alps-1.jpg" alt="mountain" /> 
+    <img src="https://dinoanimals.com/wp-content/uploads/2022/04/Alps-1.jpg" alt="mountain"className='card-img'/> 
   
-      <div className='overlay'>
+      <div className='overlay5'>
         <h5>Mountain Tops</h5>
         <p>Best place to be for peaceful minds</p>
       </div>
